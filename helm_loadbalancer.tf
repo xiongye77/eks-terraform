2571573d4db84c1045401ea41ce5caa5666e7ee6
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-04-19 21:04:56 +1000

    Create helm_loadbalancer.tf

---
+++ helm_loadbalancer.tf
@@ -0,0 +1,40 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "aws-load-balancer-controller" {
+  name       = "aws-load-balancer-controller"
+  depends_on=[null_resource.update_eks_kube_config]
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = data.aws_eks_cluster.eks_cluster.name
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "image.repository"
+    value = format("602401143452.dkr.ecr.%s.amazonaws.com/amazon/aws-load-balancer-controller",var.region)
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.4.1"
+  }
+
+  set {
+    name = "clusterSecretsPermissions.allowAllSecrets"
+    value = "true"
+  }
+
+}
